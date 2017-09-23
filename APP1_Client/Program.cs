14a4a6e39fb3c940e3a722d99b05834650077ef8
Momo60 <32074805+Momo60@users.noreply.github.com>
2017-09-23 17:30:39 -0400

    Création Client
    Création du client

---
+++ APP1_Client/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Json;
+
+namespace APP1_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenue sur notre site, Veuillez choisir le sondage!");
+            ProcessRepositories().Wait();
+            Console.ReadKey();
+		}
+
+		
+			private static async Task ProcessRepositories()
+			{
+				var client = new HttpClient();
+				client.DefaultRequestHeaders.Accept.Clear();
+				client.DefaultRequestHeaders.Accept.Add(
+					new MediaTypeWithQualityHeaderValue("application/json"));
+				client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+                var stringTask = client.GetStringAsync("https://localhost:5001/api/values");
+			    var msg = await stringTask;
+			    Console.Write(msg);
+
+		    }
+    }
+}
---
+++ APP1_Client/repo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Globalization;
+
+namespace APP1_Client
+{
+
+
+
+   
+
+    [DataContract(Name = "repo")]
+    public class Repository
+    {
+		[DataMember(Name = "name")]
+		public string Name;
+
+		[DataMember(Name = "description")]
+		public string Description { get; set; }
+
+		[DataMember(Name = "html_url")]
+		public Uri GitHubHomeUrl { get; set; }
+
+		[DataMember(Name = "homepage")]
+		public Uri Homepage { get; set; }
+
+		[DataMember(Name = "watchers")]
+		public int Watchers { get; set; }
+
+		[DataMember(Name = "pushed_at")]
+		private string JsonDate { get; set; }
+
+		[IgnoreDataMember]
+		public DateTime LastPush
+		{
+			get
+			{
+				return DateTime.ParseExact(JsonDate, "yyyy-MM-ddTHH:mm:ssZ", CultureInfo.InvariantCulture);
+			}
+		}
+
+    }
+
+
+}
