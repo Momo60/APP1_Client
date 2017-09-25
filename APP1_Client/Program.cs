d3518e1d973ab3d0f078d486828ef45ddcf6cbfb
antoneys <stephane.antoney@hotmail.fr>
2017-09-25 01:01:56 -0400

    Indentation et on ne peux plus mettre n'importe quoi

--- APP1_Client/Program.cs
+++ APP1_Client/Program.cs
@@ -3,73 +3,197 @@ using System.Threading.Tasks;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Collections.Generic;
-using System.Runtime.Serialization.Json;
-using System.Security.Cryptography.X509Certificates;
-using System.Security.Authentication;
-using System.Net.Security;
-using System.IO;
-using System.Text;
 
 namespace APP1_Client
 {
     class Program
     {
-        class ApiSendData
-        {
-            public int PollId { get; set; }
-            public int CurrentQuestionId { get; set; }
-        }
-
         static void Main(string[] args)
         {
-
             Console.Write("Bienvenue sur notre sondage\n ");
 
-            Console.Write("\nGET :\n");
+            /*Récupération de descriptifs des sondages*/
             GET().Wait();
 
-
-            Console.Write("POST :\n");
+            /*Envoie des données*/
             POST().Wait();
         }
 
-
-
-
         static async Task POST()
         {
-            string sondageId;
-            string answer;
-            string userId;
-            string apiUrl = "https://localhost:5001/api/sondage";
-            using (var client = new HttpClient()) {
-				/*******************************
-             * **** Début questionnaire ****
-             * ****************************/
+            string sondageId, answer, userId;
 
+            string apiUrl = "https://localhost:5001/api/sondage";
+
+            /*Création d'une variable HttpCLient pour les requêtes*/
+            using (var client = new HttpClient()) 
+            {
+				/*******************************
+                * **** Début questionnaire ****
+                * ****************************/
+
+                /*Mise en place du token dans l'en-tête */
 				client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2");
-				Console.Write("veuillez renseigner votre Username\n");
+
+                Console.Write("Veuillez renseigner votre userId (nombre) pour commencer : \n");
 				userId = Console.ReadLine();
 
+                bool isNumber = Int32.TryParse(userId, out int number);
+                if (isNumber == false)
+				{
+                    Console.WriteLine("Veuillez mettre un nombre :");
+                    return; /*Fin du programme*/
+				}
+
 				/*Choix N° sondage */
-				Console.Write("Choisir entre le questionnaire 1 et 2\n");
+                Console.Write("Choisir entre le sondage 1 et 2 :\n");
 				sondageId = Console.ReadLine();
 
 				if (sondageId == "1")
 				{
-
-					/**********************************************************/
-					/*1ere question*/
+					/***********************************************************
+					 *                     1ere question                       *
 					/***********************************************************/
-
 
 					/*Envoie POST pour récup sondage*/
 					var values1 = new Dictionary<string, string>()
+                    {
+					    {"PollId", sondageId},
+					    {"CurrentQuestionId", "-1"},
+					    {"test", "first"}
+				    };
+
+					var content = new FormUrlEncodedContent(values1);
+					var response1 = await client.PostAsync(apiUrl, content);
+					var result = await response1.Content.ReadAsStringAsync();
+					response1.EnsureSuccessStatusCode();
+
+					/*Affichage question*/
+					Console.Write("\n" + result);
+
+					/*Choix réponse 1ere question*/
+                    Console.Write("\nVeuillez entrez votre réponse : \n");
+					answer = Console.ReadLine();
+
+                    if (!(answer == "a" || answer == "b" || answer == "c" || answer == "d"))
+                    {
+                        Console.Write("La réponse doit être soit a, b, c ou d.\nMerci.\n");
+                        return;
+					}
+
+					/*Envoie POST réponse sondage*/
+					var values11 = new Dictionary<string, string>()
+			        {
+				        {"username", userId},
+				        {"question", answer},
+				        {"PollId", sondageId},
+				        {"CurrentQuestionId", "-1"},
+					    {"test", "second"}
+                    };
+
+					var content11 = new FormUrlEncodedContent(values11);
+					var response11 = await client.PostAsync(apiUrl, content11);
+					var result11 = await response11.Content.ReadAsStringAsync();
+					response11.EnsureSuccessStatusCode();
+
+					/***********************************************************
+                     *                     2ème question                       *
+                    /***********************************************************/
+
+					/*Affichage question 2*/
+					Console.Write("\n" + result11);
+
+					/*Choix réponse */
+                    Console.Write("\nVeuillez entrez votre réponse :\n");
+					answer = Console.ReadLine();
+
+					if (!(answer == "a" || answer == "b" || answer == "c"))
+					{
+						Console.Write("La réponse doit être soit a, b ou c.\nMerci.\n");
+						return;
+					}
+
+					/*Envoie POST réponse sondage*/
+					var values2 = new Dictionary<string, string>()
+			        {
+				        {"username", userId},
+				        {"question", answer},
+				        {"PollId", sondageId},
+				        {"CurrentQuestionId", "12"}
+                    };
+
+					var content2 = new FormUrlEncodedContent(values2);
+					var response2 = await client.PostAsync(apiUrl, content2);
+					var result2 = await response2.Content.ReadAsStringAsync();
+					response2.EnsureSuccessStatusCode();
+
+					/***********************************************************
+                     *                     3ème question                       *
+                    /***********************************************************/
+
+					/*Affichage question 3*/
+					Console.Write("\n" + result2);
+
+					/*Choix réponse */
+                    Console.Write("\nVeuillez entrez votre réponse :\n");
+					answer = Console.ReadLine();
+
+					if (!(answer == "a" || answer == "b" || answer == "c" || answer == "d"))
+					{
+						Console.Write("La réponse doit être soit a, b, c ou d.\nMerci.\n");
+						return;
+					}
+
+					/*Envoie POST réponse sondage*/
+					var values3 = new Dictionary<string, string>()
+			        {
+				        {"username", userId},
+				        {"question", answer},
+				        {"PollId", sondageId},
+				        {"CurrentQuestionId", "13"}
+                    };
+
+					var content3 = new FormUrlEncodedContent(values3);
+					var response3 = await client.PostAsync(apiUrl, content3);
+					var result3 = await response3.Content.ReadAsStringAsync();
+					response3.EnsureSuccessStatusCode();
+
+
+					/***********************************************************
+                     *                     4ème question                       *
+                    /***********************************************************/
+
+					/*Affichage question 3*/
+					Console.Write("\n" + result3);
+
+					/*Choix réponse */
+                    Console.Write("\nVeuillez entrez votre réponse :\n");
+					answer = Console.ReadLine();
+
+					if (!(answer == "a" || answer == "b" || answer == "c" || answer == "d"))
+					{
+						Console.Write("La réponse doit être soit a, b, c ou d.\nMerci.\n");
+						return;
+					}
+
+					Console.Write("Merci d'avoir répondu au sondage");
+
+				} 
+
+                else if (sondageId == "2")
+                    
 				{
-					{"PollId", sondageId},
-					{"CurrentQuestionId", "-1"},
-					{"test", "first"}
-				};
+					/***********************************************************
+                     *                     1ere question                       *
+                    /***********************************************************/
+
+					/*Envoie POST pour récup sondage*/
+					var values1 = new Dictionary<string, string>()
+				    {
+					    {"PollId", sondageId},
+					    {"CurrentQuestionId", "-1"},
+					    {"test", "first"}
+				    };
 
 					var content = new FormUrlEncodedContent(values1);
 					var response1 = await client.PostAsync(apiUrl, content);
@@ -81,74 +205,87 @@ namespace APP1_Client
 					Console.Write("\n" + result);
 
 					/*Choix réponse 1ere question*/
-					Console.Write("\nVeuillez entrez votre réponse \n");
+                    Console.Write("\nVeuillez entrez votre réponse :\n");
 					answer = Console.ReadLine();
 
+					if (!(answer == "a" || answer == "b" || answer == "c" || answer == "d"))
+					{
+						Console.Write("La réponse doit être soit a, b, c ou d.\nMerci.\n");
+						return;
+					}
 
 
 					/*Envoie POST réponse sondage*/
 					var values11 = new Dictionary<string, string>()
-			{
-				{"username", userId},
-				{"question", answer},
-				{"PollId", sondageId},
-				{"CurrentQuestionId", "-1"},
-					{"test", "second"}
-
-			};
+			        {
+				        {"username", userId},
+				        {"question", answer},
+				        {"PollId", sondageId},
+				        {"CurrentQuestionId", "-1"},
+					    {"test", "second"}
+			        };
 
 					var content11 = new FormUrlEncodedContent(values11);
 					var response11 = await client.PostAsync(apiUrl, content11);
 					var result11 = await response11.Content.ReadAsStringAsync();
 					response11.EnsureSuccessStatusCode();
 
-					/**************************************************************/
-					/*Question 2*/
-					/**************************************************************/
+					/***********************************************************
+                     *                     2ème question                       *
+                    /***********************************************************/
 
 					/*Affichage question 2*/
 					Console.Write("\n" + result11);
 
 					/*Choix réponse */
-					Console.Write("\nVeuillez entrez votre réponse \n");
+                    Console.Write("\nVeuillez entrez votre réponse :\n");
 					answer = Console.ReadLine();
+
+					if (!(answer == "a" || answer == "b" || answer == "c"))
+					{
+						Console.Write("La réponse doit être soit a, b ou c.\nMerci.\n");
+						return;
+					}
 
 					/*Envoie POST réponse sondage*/
 					var values2 = new Dictionary<string, string>()
-			{
-				{"username", userId},
-				{"question", answer},
-				{"PollId", sondageId},
-				{"CurrentQuestionId", "12"},
-
-			};
+			        {
+				        {"username", userId},
+				        {"question", answer},
+				        {"PollId", sondageId},
+				        {"CurrentQuestionId", "22"}
+			        };
 
 					var content2 = new FormUrlEncodedContent(values2);
 					var response2 = await client.PostAsync(apiUrl, content2);
 					var result2 = await response2.Content.ReadAsStringAsync();
 					response2.EnsureSuccessStatusCode();
 
-					/**************************************************************/
-					/*Question 3*/
-					/**************************************************************/
+					/***********************************************************
+                     *                     3ème question                       *
+                    /***********************************************************/
 
 					/*Affichage question 3*/
 					Console.Write("\n" + result2);
 
 					/*Choix réponse */
-					Console.Write("\nVeuillez entrez votre réponse \n");
+                    Console.Write("\nVeuillez entrez votre réponse :\n");
 					answer = Console.ReadLine();
 
+					if (!(answer == "a" || answer == "b" || answer == "c" || answer == "d"))
+					{
+						Console.Write("La réponse doit être soit a, b, c ou d.\nMerci.\n");
+						return;
+					}
 
 					/*Envoie POST réponse sondage*/
 					var values3 = new Dictionary<string, string>()
-			{
-				{"username", userId},
-				{"question", answer},
-				{"PollId", sondageId},
-				{"CurrentQuestionId", "13"},
-
-			};
+			        {
+				        {"username", userId},
+				        {"question", answer},
+				        {"PollId", sondageId},
+				        {"CurrentQuestionId", "23"}
+			        };
 
 					var content3 = new FormUrlEncodedContent(values3);
 					var response3 = await client.PostAsync(apiUrl, content3);
@@ -156,147 +293,39 @@ namespace APP1_Client
 					response3.EnsureSuccessStatusCode();
 
 
-					/**************************************************************/
-					/*Question 4*/
-					/**************************************************************/
+					/***********************************************************
+                     *                     4ème question                       *
+                    /***********************************************************/
 
 					/*Affichage question 3*/
 					Console.Write("\n" + result3);
 
 					/*Choix réponse */
-					Console.Write("\nVeuillez entrez votre réponse \n");
+                    Console.Write("\nVeuillez entrez votre réponse :\n");
 					answer = Console.ReadLine();
+
+					if (!(answer == "a" || answer == "b" || answer == "c" || answer == "d"))
+					{
+						Console.Write("La réponse doit être soit a, b, c ou d.\nMerci.\n");
+						return;
+					}
 
 					Console.Write("Merci d'avoir répondu au sondage");
+                } 
 
+                else 
+                
+                {
+                    Console.Write("Sondage indisponible.\n");
+					Console.Write("Veuillez choisir entre 1 et 2.\n");
+                    return;
 				}
-
-				if (sondageId == "2")
-				{
-
-					/**********************************************************/
-					/*1ere question*/
-					/***********************************************************/
-
-
-					/*Envoie POST pour récup sondage*/
-					var values1 = new Dictionary<string, string>()
-				{
-					{"PollId", sondageId},
-					{"CurrentQuestionId", "-1"},
-					{"test", "first"}
-				};
-
-					var content = new FormUrlEncodedContent(values1);
-					var response1 = await client.PostAsync(apiUrl, content);
-					var result = await response1.Content.ReadAsStringAsync();
-					response1.EnsureSuccessStatusCode();
-
-
-					/*Affichage question*/
-					Console.Write("\n" + result);
-
-					/*Choix réponse 1ere question*/
-					Console.Write("\nVeuillez entrez votre réponse \n");
-					answer = Console.ReadLine();
-
-
-
-					/*Envoie POST réponse sondage*/
-					var values11 = new Dictionary<string, string>()
-			{
-				{"username", userId},
-				{"question", answer},
-				{"PollId", sondageId},
-				{"CurrentQuestionId", "-1"},
-					{"test", "second"}
-
-			};
-
-					var content11 = new FormUrlEncodedContent(values11);
-					var response11 = await client.PostAsync(apiUrl, content11);
-					var result11 = await response11.Content.ReadAsStringAsync();
-					response11.EnsureSuccessStatusCode();
-
-					/**************************************************************/
-					/*Question 2*/
-					/**************************************************************/
-
-					/*Affichage question 2*/
-					Console.Write("\n" + result11);
-
-					/*Choix réponse */
-					Console.Write("\nVeuillez entrez votre réponse \n");
-					answer = Console.ReadLine();
-
-					/*Envoie POST réponse sondage*/
-					var values2 = new Dictionary<string, string>()
-			{
-				{"username", userId},
-				{"question", answer},
-				{"PollId", sondageId},
-				{"CurrentQuestionId", "22"},
-
-			};
-
-					var content2 = new FormUrlEncodedContent(values2);
-					var response2 = await client.PostAsync(apiUrl, content2);
-					var result2 = await response2.Content.ReadAsStringAsync();
-					response2.EnsureSuccessStatusCode();
-
-					/**************************************************************/
-					/*Question 3*/
-					/**************************************************************/
-
-					/*Affichage question 3*/
-					Console.Write("\n" + result2);
-
-					/*Choix réponse */
-					Console.Write("\nVeuillez entrez votre réponse \n");
-					answer = Console.ReadLine();
-
-
-					/*Envoie POST réponse sondage*/
-					var values3 = new Dictionary<string, string>()
-			{
-				{"username", userId},
-				{"question", answer},
-				{"PollId", sondageId},
-				{"CurrentQuestionId", "23"},
-
-			};
-
-					var content3 = new FormUrlEncodedContent(values3);
-					var response3 = await client.PostAsync(apiUrl, content3);
-					var result3 = await response3.Content.ReadAsStringAsync();
-					response3.EnsureSuccessStatusCode();
-
-
-					/**************************************************************/
-					/*Question 4*/
-					/**************************************************************/
-
-					/*Affichage question 3*/
-					Console.Write("\n" + result3);
-
-					/*Choix réponse */
-					Console.Write("\nVeuillez entrez votre réponse \n");
-					answer = Console.ReadLine();
-
-					Console.Write("Merci d'avoir répondu au sondage");
-
-				}
-
-
 			}
-
-
-           
         }
 
-       				
-
-
+        /*******************************************************
+         * Méthode pour récupérer les descriptifs des sondages *
+         * ****************************************************/
         static async Task GET()
 		{
 			using (var clientHandlerGET = new HttpClientHandler())
@@ -305,15 +334,15 @@ namespace APP1_Client
 				{
 					clientGET.DefaultRequestHeaders.Accept.Clear();
 					clientGET.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                    clientGET.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2");
+
+					/*Mise en place du token dans l'en-tête */
+					clientGET.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2");
 
                     var stringContent = clientGET.GetStringAsync("https://localhost:5001/api/sondage");
 					var message = await stringContent;
-					Console.Write("\nVoici les sondages disponible\n" + message + "\n");
-
-				
+                    Console.Write("\nVoici les sondages disponibles :\n" + message + "\n");
+                    Console.Write(("\n"));
 				}
-
 
 			}
 		}
